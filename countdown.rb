955c4440d3a5e4ecf63c4b9c473f3cd20275e4e4
Adrian <adrianic92@yahoo.com>
2023-01-07 09:31:46 -0800

    commit tree b732669ad6618aa57783184fc50ee9c56d78e58e

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(int)
+    x = int
+    while x >= 1
+        puts "#{x} SECOND(S)!"
+        x = x - 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+    x = int
+    while x >= 1
+        puts "#{x} SECOND(S)!"
+        x = x - 1
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!"
 end
